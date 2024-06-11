24236091a7a217899f375b812b63f8fa7873ff71
Muhammad Ismail Rasheed <159624882+MIR00291341@users.noreply.github.com>
2024-06-11 22:34:03 +0500

    Create assignment#2.py
    ASSIGNMENT

---
+++ assignment#2.py
@@ -0,0 +1,21 @@
+#Calculate the area of a triangle using the formula (0.5 ​× base x height).
+
+base = 4
+height = 8
+areaOfAtringle = 0.5*base*height
+print("the area of a triangle is", areaOfAtringle)
+
+#Compute the area of a square using the formula (side x side).
+
+sideA = 5
+SideB = 5
+square = sideA*SideB
+print("the area of a square is",square)
+
+#Find the area of a circle with a radius of 7 units. formula (π×radius*radius).
+
+radius = 7
+π = 3.14159
+area = π * radius * radius
+
+print("The area of the circle is:", area)
